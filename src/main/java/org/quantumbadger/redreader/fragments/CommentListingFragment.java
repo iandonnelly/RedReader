c02b02dfb55260ca530d29fcc5f21e8e0c1b65ab
Ian Donnelly <easyxtarget@gmail.com>
2015-04-13 17:48:51 -0400

    Actually close app

--- src/main/java/org/quantumbadger/redreader/fragments/CommentListingFragment.java
+++ src/main/java/org/quantumbadger/redreader/fragments/CommentListingFragment.java
@@ -510,7 +510,7 @@ public class CommentListingFragment extends Fragment
 
         // Code to close app
         getActivity().finish();
-        System.exit(0);
+		android.os.Process.killProcess(android.os.Process.myPid());
 	}
 
 	private final ArrayList<RedditCommentListItem> mItemBuffer = new ArrayList<RedditCommentListItem>(64);
